f93ccb6794aa0bb211911b7244890f94b4c0e4f0
Arnaldo Martins Hidalgo Junior <arnaldo@hidalgojunior.com.br>
2022-04-26 18:03:04 -0300

    Primeiro arquivo do projeto de músicas
    Trabalhando com MaterializeCss

---
+++ index.php
@@ -0,0 +1,196 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Musicas - EE Sebastião Monaco</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+</head>
+
+<body>
+    <div class="container">
+        <nav>
+            <div class="nav-wrapper brown darken-1">
+                <a href="#!" class="brand-logo center">Logo</a>
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="sass.html">Sass</a></li>
+                    <li><a href="badges.html">Components</a></li>
+                    <li><a href="collapsible.html">Javascript</a></li>
+                    <li><a href="mobile.html">Mobile</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <ul class="sidenav" id="mobile-demo">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">Javascript</a></li>
+            <li><a href="mobile.html">Mobile</a></li>
+        </ul>
+    </div>
+    <div class="container">
+
+    <div class="carousel carousel-slider">
+    <a class="carousel-item" href="#one!"><img src="https://picsum.photos/800/400"></a>
+    <a class="carousel-item" href="#two!"><img src="https://picsum.photos/800/400"></a>
+    <a class="carousel-item" href="#three!"><img src="https://picsum.photos/800/400"></a>
+    <a class="carousel-item" href="#four!"><img src="https://picsum.photos/800/400"></a>
+  </div>
+    
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col s12 m6 ">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="https://picsum.photos/200">
+                        <span class="card-title">Discografia</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6 ">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="https://picsum.photos/200">
+                        <span class="card-title">Discografia</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6 ">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="https://picsum.photos/200">
+                        <span class="card-title">Discografia</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6 ">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="https://picsum.photos/200">
+                        <span class="card-title">Discografia</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6 ">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="https://picsum.photos/200">
+                        <span class="card-title">Discografia</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="https://picsum.photos/200">
+                        <span class="card-title">Discografia</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6 ">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="https://picsum.photos/200">
+                        <span class="card-title">Discografia</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                            I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">This is a link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        
+    <footer class="page-footer brown darken-3">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © <?=date('Y')?>
+             <a class="white-text" href="mailto:arnaldo.hidalgo@etec.sp.gov.br">
+                 Arnaldo Martins Hidalgo Junior
+             </a>
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <script type="text/javascript">
+    M.AutoInit();
+    </script>
+</body>
+
+</html>
